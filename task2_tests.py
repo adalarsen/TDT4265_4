79411aeaa8c459ad2a27bcbdeae33d5eab6d3aef
hellkyb <ohelbak@gmail.com>
2019-03-07 15:33:27 +0100

    Previous file, bug fix

--- task2_tests.py
+++ task2_tests.py
@@ -203,13 +203,13 @@ def test_mean_average_precision():
 
 
 if __name__ == "__main__":
-    #test_iou()
+    test_iou()
     test_precision()
     test_recall()
-    #test_get_all_box_matches()
-    #test_calculate_individual_image_result()
-    #test_calculate_precision_recall_all_images()
-    #test_get_precision_recall_curve()
+    test_get_all_box_matches()
+    test_calculate_individual_image_result()
+    test_calculate_precision_recall_all_images()
+    test_get_precision_recall_curve()
     #test_mean_average_precision()
     print("="*80)
     print("All tests OK.")
